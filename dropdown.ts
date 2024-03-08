a487b78b84b61d30e448eb9abb9d08e9d67dfd48
Muthu <muthusrinivasan.a@gmail.com>
2024-03-08 12:02:23 +0530

    Update dropdown.ts

--- dropdown.ts
+++ dropdown.ts
@@ -1,4 +1,81 @@
 import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { DropdownItem } from './dropdown-item';
+
+interface DropdownItem {
+  id: number | string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-multi-select-dropdown',
+  templateUrl: './multi-select-dropdown.component.html',
+  styleUrls: ['./multi-select-dropdown.component.scss']
+})
+export class MultiSelectDropdownComponent implements OnInit {
+  @Input() items: DropdownItem[] = [];
+  @Input() showSearch: boolean = true;
+  @Input() defaultSelected: DropdownItem[] = [];
+  @Output() selectedItemsChange = new EventEmitter<DropdownItem[]>();
+
+  filteredItems: DropdownItem[] = [];
+  isDropdownOpen: boolean = false;
+  isDropdownTop: boolean = false;
+  selectedItems: Set<number | string> = new Set();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.filteredItems = [...this.items];
+    this.defaultSelected.forEach(item => this.selectedItems.add(item.id));
+    this.emitSelectedItems();
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    this.checkDropdownPosition();
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      this.closeDropdown();
+    }
+  }
+
+  filterItems(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.filteredItems = this.items.filter(item => item.name.toLowerCase().includes(query));
+  }
+
+  onItemSelect(event: any, item: DropdownItem) {
+    if (event.target.checked) {
+      this.selectedItems.add(item.id);
+    } else {
+      this.selectedItems.delete(item.id);
+    }
+    this.emitSelectedItems();
+  }
+
+  emitSelectedItems() {
+    const selectedItemsArray = this.items.filter(item => this.selectedItems.has(item.id));
+    this.selectedItemsChange.emit(selectedItemsArray);
+  }
+
+  checkDropdownPosition() {
+    const dropdownRect = document.querySelector('.dropdown')?.getBoundingClientRect();
+    this.isDropdownTop = dropdownRect ? window.innerHeight - dropdownRect.bottom < 200 : false;
+  }
+}
+
+
+
+/* OLD */
+/* import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 interface DropdownItem {
   id: number | string;
@@ -64,4 +141,4 @@ export class MultiSelectDropdownComponent implements OnInit {
     const dropdownRect = document.querySelector('.dropdown')?.getBoundingClientRect();
     this.isDropdownTop = dropdownRect ? window.innerHeight - dropdownRect.bottom < 200 : false;
   }
-}
+} */
